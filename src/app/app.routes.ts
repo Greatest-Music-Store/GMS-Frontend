32420c60e53a084bf5a981736dab77c308d99b29
vifeijo <vibfeijo@gmail.com>
2026-06-12 11:01:46 -0300

    feat: register ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login'
+import { Register } from './features/auth/pages/register/register';
 import { Home } from './features/home/home'
 import { MainLayout } from './layouts/main-layout/main-layout';
+import { register } from 'module';
+
 
 
 export const routes: Routes = [
@@ -10,6 +13,11 @@ export const routes: Routes = [
         path: 'login',
         title: 'login page',
         component: Login,
+    },
+    {
+        path: 'register',
+        title: 'cadastrar-se',
+        component: Register,
     },
     {
         path: '',
--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { tdesignMail, tdesignLockOn,  tdesignUser, tdesignAssignmentUserFilled } from '@ng-icons/tdesign-icons'
+import { NgIcon, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  viewProviders: [provideIcons({ tdesignLockOn, tdesignMail,  tdesignUser, tdesignAssignmentUserFilled })],
 })
 export class Register {}
